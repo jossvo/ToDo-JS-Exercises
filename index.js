03a4b999cbccffc33116bae22b878aa64a18c338
jossvo <jossvo26@outlook.com>
2022-12-06 04:22:24 +0000

    Copy of ToDo Exercise

---
+++ index.js
@@ -0,0 +1,36 @@
+// Your code here
+let toDoList = document.querySelector("#ToDoActivities")
+let toDoInput = document.querySelector("#addToDo")
+
+window.addEventListener("load", (event) => {
+    test()
+});
+
+toDoInput.addEventListener("keyup", function(event) {
+    if (event.key === "Enter" && toDoInput.value!="" ) {
+        let toDoAct = document.createElement("li")
+        toDoAct.innerHTML=`<span><i class="fa fa-trash"></i></span> ${toDoInput.value}`
+        toDoList.appendChild(toDoAct)
+        toDoInput.value = "";
+        test()
+    }
+})
+
+let trashIcons = document.getElementsByClassName("fa fa-trash");
+let trashBackground = document.getElementsByTagName("span");
+function test(){
+    for(let i=0;i<trashIcons.length;i++){
+        trashIcons[i].addEventListener("click",(event)=>{
+            event.target.parentElement.parentElement.remove()
+            event.preventDefault();
+        })
+    }
+    for(let i=0;i<trashBackground.length;i++){
+        trashBackground[i].addEventListener("click",(event)=>{
+            event.target.parentElement.remove()
+            event.preventDefault();
+        })
+    }
+}
+
+
